f78b0b84c6359892e99f659973a2d54dfbf24d51
keshavdulal <keshav.dulal@gmail.com>
2020-11-25 22:42:06 +0545

    add(sidebar): conversation & contacts comp

---
+++ client/src/components/Contacts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Contacts = () => {
+  return (
+    <span>This is Contacts</span>
+  );
+}
+
+export default Contacts;
---
+++ client/src/components/Conversations.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Conversations = () => {
+  return (
+    <span>This is Conversations</span>
+  );
+}
+
+export default Conversations;
