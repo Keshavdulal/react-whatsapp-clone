c8641e2fe41ba2de540a80a41366827873867b8e
keshavdulal <keshav.dulal@gmail.com>
2020-12-11 11:43:07 +0545

    add id based login

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 import Login from "./Login";
 import useLocalStorage from '../hooks/useLocalStorage';
+import Dashboard from "./Dashboard";
+import {ContactsProvider} from "../context/ContactsProvider"
+import { ConversationProvider } from '../context/ConversationProvider';
 
 function App() {
   const [id, setId] = useLocalStorage("id")
+
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationProvider>
+        <Dashboard id={id}/>
+      </ConversationProvider>
+    </ContactsProvider>
+  )
   
   return (
-    <>
-      {id}
-      <Login onIdSubmit={setId}/>
-    </>
-  );
+    id ? dashboard : <Login onIdSubmit={setId}/>
+    )
 }
 
 export default App;
